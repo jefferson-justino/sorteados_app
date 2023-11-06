86558e5e90464bcfac95997901dbb9fcbe1fafd2
LucasQueiroz1 <lucasqueiroz34@gmail.com>
2023-11-06 11:12:36 -0300

    Identação das telas de Sorteio de Nome e Número

--- src/screens/ScreenNumber.js
+++ src/screens/ScreenNumber.js
@@ -35,67 +35,81 @@ export default function ScreenNumber() {
 
   return (
     <View style={styles.container}>
-      <View >
-        <Text style={styles.text}>Quantidade de participantes:</Text>
-      </View>
-
-      <View style={styles.amount}>
-        <TextInput
-          placeholder="Ex: 20"
-          selectionColor="#000000"
-          placeholderTextColor="#B6B6B6"
-          keyboardType='numeric'
-          onChangeText={setQuantidade}
-
-        />
-      </View>
-
-      <DefaultButton
-        buttonText={'SORTEAR'}
-        backgroundColor={'#03575C'}
-        marginTop={14}
-        click={numero}
-        width={200}
-        height={50}
-        marginBottom={20}
-      />
-
-      <View>
-        <Text style={styles.text}>{!num ? '' : 'O NúMERO SORTEADOR FOI:'}</Text>
-      </View>
-
-      <View>
-        <Text style={styles.result}>{!num ? '' : num}</Text>
-      </View>
-
-      <DefaultButton
-        buttonText={'SALVAR RESULTADO'}
-        backgroundColor={'#03575C'}
-        marginTop={14}
-        click={() => setModalVisible(!modalVisible)}
-        width={200}
-        height={50}
-      />
-      
-      <Modal visible={modalVisible} animationType="slide" transparent={true}
+      <ImageBackground
+        source={require('../Assets/fundo.png')}
+        resizeMode='cover'
+        style={{ flex: 1, justifyContent: 'center' }}
       >
-        <View style={styles.tamModal}>
-          <View style={styles.centralModal}>
-            <TextInput placeholder='Digite o título do sorteio' onChangeText={setTitulo}
-              style={styles.amount} maxLength={25} />
-            <DefaultButton
-              buttonText={'SALVAR '}
-              backgroundColor={'#03575C'}
-              marginTop={14}
-              click={salvar}
-              width={200}
-              height={50}
+
+        <View style={styles.center}>
+          <Text style={styles.text}>Quantidade de participantes:</Text>
+        </View>
+
+        <View style={styles.center}>
+          <View style={styles.amount}>
+            <TextInput
+              placeholder="Ex.: 20"
+              selectionColor="#000000"
+              placeholderTextColor="#B6B6B6"
+              keyboardType='numeric'
+              onChangeText={setQuantidade}
             />
           </View>
         </View>
+        
+        <View style={styles.center}>
+          <DefaultButton
+            buttonText={'SORTEAR'}
+            backgroundColor={'#00BF63'}
+            marginTop={14}
+            click={numero}
+            width={200}
+            height={50}
+            marginBottom={20}
+          />
+        </View>
+        
+        <View style={styles.center}>
+          <Text style={styles.text}>{!num ? '' : 'O NúMERO SORTEADOR FOI:'}</Text>
+        </View>
 
-      </Modal>
+        <View style={styles.center}>
+          <Text style={styles.result}>{!num ? '' : num}</Text>
+        </View>
 
+        <View style={styles.center}>
+          <DefaultButton
+            buttonText={'SALVAR RESULTADO'}
+            backgroundColor={'#00BF63'}
+            marginTop={14}
+            click={() => setModalVisible(!modalVisible)}
+            width={200}
+            height={50}
+          />
+        </View>
+
+        <Modal 
+          visible={modalVisible} 
+          animationType="slide" 
+          transparent={true}
+        >
+          <View style={styles.tamModal}>
+            <View style={styles.centralModal}>
+              <TextInput placeholder='Digite o título do sorteio' onChangeText={setTitulo}
+                style={styles.amount} maxLength={25} />
+              <DefaultButton
+                buttonText={'SALVAR '}
+                backgroundColor={'#00BF63'}
+                marginTop={14}
+                click={salvar}
+                width={200}
+                height={50}
+              />
+            </View>
+          </View>
+
+        </Modal>
+      </ImageBackground>
     </View>
   );
 }
@@ -103,9 +117,6 @@ export default function ScreenNumber() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   centralModal: {
     margin: 20,
@@ -132,20 +143,24 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     padding: 15,
-    color: "#03575C"
+    color: "#fff",
   },
   amount: {
     width: "15%",
     alignItems: 'center',
     borderWidth: 2,
     borderRadius: 8,
+    borderColor: "#fff",
     padding: 5,
-    marginBottom: 20
+    marginBottom: 20,
   },
   result: {
     fontSize: 30,
     fontWeight: "bold",
     color: "#000000",
     fontStyle: "italic"
+  },
+  center: {
+    alignItems: "center",
   }
 });
